12f3e4620f973760de8e7a4c502ba869688cd66f
mugilane.cse <55045435+Mugilane@users.noreply.github.com>
2024-07-15 21:04:38 +0530

    gert

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,11 @@ function App() {
       <h1>CGPA Calculator Apk</h1>
       <p>TechCos CGPA Calculator application Download page</p>
       <a className='btn btn-success' href="https://github.com/TechCos-Inc/CGPA-apk-downloaded/raw/main/public/CGPA_Calculator.apk" download="CGPA_Calculator.apk"> Direct Download</a>
-
     </div>
-      
+    <div className="cen container-fluid">
+    <h4>Discription</h4>
+    <p>If dialgue box please click install anyway</p>
+    </div>
     </>
   )
 }
