557f83ccdd59dd82f7b8237dd83d5f91449d3e1a
mugilane.cse <55045435+Mugilane@users.noreply.github.com>
2024-07-15 20:57:04 +0530

    few

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,8 @@ function App() {
     <div className="detial container-fluid">
       <h1>CGPA Calculator Apk</h1>
       <p>TechCos CGPA Calculator application Download page</p>
-      <a className='btn btn-success' href=" https://TechCos-Inc.github.io/CGPA-apk-downloaded/CGPA_Calculator.apk" download="CGPA_Calculator.apk">Download</a>
+      <a className='btn btn-success' href=" https://TechCos-Inc.github.io/CGPA-apk-downloaded/public/CGPA_Calculator.apk" download="CGPA_Calculator.apk"> Direct Download</a>
+      <h5>OR</h5>
     </div>
       
     </>
